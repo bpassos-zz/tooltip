9715591afdc0203e2a2dc33b2e1e8cda475ede29
Bruno Passos <bruno.passos@booking.com>
2014-03-14 21:18:38 +0100

    Simple tooltip initial commit

---
+++ js/script.js
@@ -0,0 +1,17 @@
+$(document).ready( function() {
+
+	$(".square").mousemove(function(e) { 
+		var tooltipSelector = $(this).attr('data-tooltip-content');
+		var $tooltip = $(tooltipSelector);
+
+		$tooltip.css({
+			'top': e.pageY - 20,
+			'left': e.pageX + 10
+		}).show();
+    });
+
+    // Remove tooltip when mouseout
+    $(".square").mouseout(function() { 
+        $('.tooltip').css('display', 'none');
+    });
+});
